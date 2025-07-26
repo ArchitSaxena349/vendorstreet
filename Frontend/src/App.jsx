45e3fda5d2a5742f17445e9c7a1301f2cdb8c194
Ankit Sharma <ankitengineer@hotmail.com>
2025-07-26 20:00:42 +0530

    project initialised

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Projet Started</h1>
+    </>
+  )
+}
+
+export default App
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('server is running on port 3000')
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+});
